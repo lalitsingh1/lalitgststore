c332021eefa29f75bc09b48ae5e6c6a8e64dbd67
lsrbanna <64417115+lsrbanna@users.noreply.github.com>
2020-05-18 14:29:11 +0530

    done

---
+++ GSTfinal/src/main/java/com/example/demo/GsTfinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsTfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsTfinalApplication.class, args);
+	}
+
+}
